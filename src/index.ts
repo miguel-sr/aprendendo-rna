67871625e404c5b049ed5fd3b9664a34dc634658
Miguel S. Ramos <miguel.ramos@inventsoftware.com.br>
2023-06-23 11:12:36 -0300

    multiple neural networks

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,11 @@
+import NeuralNetwork from "./neuralNetwork";
 import generateRandomInputs from "./utils/generateRandomInputs";
-import myNetwork from "./neuralNetwork";
-
-enum Emotions {
-  Sad = 0,
-  Neutral = 0.5,
-  Happy = 1,
-}
-
-const learningRate = 0.3;
-
-for (let i = 0; i < 20000; i++) {
-  // 0,0 => 0
-  myNetwork.activate([Emotions.Sad, Emotions.Sad]);
-  myNetwork.propagate(learningRate, [Emotions.Sad]);
-  // 0,1 => 0.5
-  myNetwork.activate([Emotions.Sad, Emotions.Happy]);
-  myNetwork.propagate(learningRate, [Emotions.Neutral]);
-  // 0,1 => 0.5
-  myNetwork.activate([Emotions.Happy, Emotions.Sad]);
-  myNetwork.propagate(learningRate, [Emotions.Neutral]);
-  // 1,1 => 1
-  myNetwork.activate([Emotions.Happy, Emotions.Happy]);
-  myNetwork.propagate(learningRate, [Emotions.Happy]);
-}
 
 const randomInput = generateRandomInputs();
 
-randomInput.forEach((input) => {
-  const output = myNetwork.activate(input)[0];
+const networks = [new NeuralNetwork(), new NeuralNetwork()];
 
-  switch (parseFloat(output.toFixed(1))) {
-    case 0.0:
-      console.log({ Sad: output });
-      break;
-
-    case 0.5:
-      console.log({ Neutral: output });
-      break;
-
-    case 1.0:
-      console.log({ Happy: output });
-      break;
-  }
+networks.forEach((network) => {
+  network.trainNetwork();
+  console.log(network.getResults(randomInput));
 });
--- src/neuralNetwork.ts
+++ src/neuralNetwork.ts
@@ -13,4 +13,54 @@ const myNetwork = new Network({
   output: outputLayer,
 });
 
-export default myNetwork;
+enum Emotions {
+  Sad = 0,
+  Neutral = 0.5,
+  Happy = 1,
+}
+
+const learningRate = 0.3;
+
+class NeuralNetwork {
+  getResults(inputs: number[][]): object[] {
+    const results: object[] = [];
+
+    inputs.forEach((input) => {
+      const output = myNetwork.activate(input)[0];
+      switch (parseFloat(output.toFixed(1))) {
+        case 0.0:
+          results.push({ Sad: output });
+          break;
+
+        case 0.5:
+          results.push({ Neutral: output });
+          break;
+
+        case 1.0:
+          results.push({ Happy: output });
+          break;
+      }
+    });
+
+    return results;
+  }
+
+  public trainNetwork() {
+    for (let i = 0; i < 20000; i++) {
+      // 0,0 => 0
+      myNetwork.activate([Emotions.Sad, Emotions.Sad]);
+      myNetwork.propagate(learningRate, [Emotions.Sad]);
+      // 0,1 => 0.5
+      myNetwork.activate([Emotions.Sad, Emotions.Happy]);
+      myNetwork.propagate(learningRate, [Emotions.Neutral]);
+      // 0,1 => 0.5
+      myNetwork.activate([Emotions.Happy, Emotions.Sad]);
+      myNetwork.propagate(learningRate, [Emotions.Neutral]);
+      // 1,1 => 1
+      myNetwork.activate([Emotions.Happy, Emotions.Happy]);
+      myNetwork.propagate(learningRate, [Emotions.Happy]);
+    }
+  }
+}
+
+export default NeuralNetwork;
